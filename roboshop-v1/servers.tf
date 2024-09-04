d446598d5c0d717e64f52bc79252a1ca9af5ae89
Vinith Amma <vinithamma007@gmail.com>
2024-09-04 19:06:42 +0530

    server creation

---
+++ roboshop-v1/servers.tf
@@ -0,0 +1,94 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "frontend"
+  }
+}
+resource "aws_instance" "mongodb" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "mongodb"
+  }
+}
+resource "aws_instance" "catalogue" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "catalogue"
+  }
+}
+resource "aws_instance" "user" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "user"
+  }
+}
+resource "aws_instance" "cart" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "cart"
+  }
+}
+
+resource "aws_instance" "redis" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "redis"
+  }
+}
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_instance" "shipping" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "shipping"
+  }
+}
+
+resource "aws_instance" "rabbitmq" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+
+resource "aws_instance" "payment" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "payment"
+  }
+}
+
+resource "aws_instance" "dispatch" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "dispatch"
+  }
+}
