adc7ca85a22c99664dadc2663bab975d2ac89506
Vinith Amma <vinithamma007@gmail.com>
2024-09-06 13:04:59 +0530

    map of map  var

---
+++ variables/04.mapofmap-vars.tf
@@ -0,0 +1,20 @@
+variable "fruit_stock_with_price" {
+  default = {
+    apple = {
+      stock = 100
+      price = 5
+      for_sale = true
+      location = "warangal"
+    }
+    banana = {
+      stock = 200
+      price = 2
+      for_sale = true
+      location = "hanumakonda"
+    }
+  }
+}
+
+output "fruit_stock" {
+  value = var.fruit_stock_with_price["apple"].price
+}
