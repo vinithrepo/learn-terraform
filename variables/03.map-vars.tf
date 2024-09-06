01b5d39724fac9175ded102611b313ff627f8e3d
Vinith Amma <vinithamma007@gmail.com>
2024-09-06 08:48:32 +0530

    map  var

--- variables/03.map-vars.tf
+++ variables/03.map-vars.tf
@@ -2,6 +2,7 @@ variable "fruit_stock" {
   default = {
     apple = 100
     banana = "twenty"
+    guva = 300
 
   }
 }
@@ -10,5 +11,5 @@ output "fruit_stock_apple" {
   value = var.fruit_stock["apple"]
 }
 output "fruit_stock_banana" {
-  value = var.fruit_stock["banana"]
+  value = " ${var.fruit_stock["banana"]} , ${var.fruit_stock["guva"]}"
 }
