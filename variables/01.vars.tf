5d636972e3e8ab31fb4d3017253c7abbebbd036d
Vinith Amma <vinithamma007@gmail.com>
2024-09-06 08:09:58 +0530

    list var

--- variables/01.vars.tf
+++ variables/01.vars.tf
@@ -1,3 +1,4 @@
+//ex1
 variable "name" {
   default = "vinith"
 }
@@ -5,18 +6,21 @@ variable "name" {
 output "my_name_is" {
   value = var.name
 }
+//ex2
 variable "fruit_name" {
   default = "Orange"
 }
 output "fruit_name_is" {
   value = var.fruit_name
 }
+//ex3
 variable "item_name" {
   default = "mobile"
 }
 output "item" {
   value = " name is = ${var.item_name} "
 }
+//ex4
 variable "item_name_is" {
   default = "cellphone"
 }
--- variables/02.list-vars.tf
+++ variables/02.list-vars.tf
@@ -1,3 +1,4 @@
+//ex1//
 variable "fruits" {
   default = [
   "apple", #comma should be here to separate the item
@@ -8,10 +9,16 @@ variable "fruits" {
 output "fruits_first" {
   value = var.fruits[0]
 }
+
+//ex2//
 variable "fruits_name" {
   default = [ "apple", "banana" ]
 }
 
 output "fruits_second" {
   value = var.fruits_name[1]
+}
+
+output "fruits_name" {
+  value = "${ var.fruit_name[0]}"
 }
