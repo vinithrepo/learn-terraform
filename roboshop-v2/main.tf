eda07950439cf5baff0dfd781b846d63c5ed8857
Vinith Amma <vinithamma007@gmail.com>
2024-09-08 20:03:29 +0530

    for each

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -10,189 +10,36 @@ variable "security_groups" {
 variable "zone_id" {
   default = "Z0345275C3S6UDSOR4CU"
 }
+variable "components" {
+  default = {
+    frontend = { name = "frontend-dev" }
+    mongodb = { name = "mongodb-dev" }
+    catalogue = { name = "catalogue-dev" }
+    user = { name = "user-dev" }
+    cart = { name = "cart-dev" }
+    redis = { name = "redis-dev" }
+    mysql = { name = "mysql-dev" }
+    shipping = { name = "shipping-dev" }
+    rabbitmq = { name = "rabbitmq-dev" }
+    payment = { name = "payment-dev" }
+  }
+}
 
 
-resource "aws_instance" "frontend" {
+resource "aws_instance" "instance" {
+  for_each = var.components
   ami           = var.ami
   instance_type = var.instance_type
   vpc_security_group_ids = var.security_groups
 
   tags = {
-    Name = "frontend"
+    Name = lookup(each.value,"name", null)
   }
 }
 resource "aws_route53_record" "frontend" {
   zone_id = var.zone_id
-  name    = "frontend-dev.vinithaws.online"
+  name    = "${lookup(each.value, "name", null)}.vinithaws.online"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.frontend.private_ip]
-}
-
-resource "aws_instance" "mongodb" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.security_groups
-
-  tags = {
-    Name = "mongodb"
-  }
-}
-resource "aws_route53_record" "mongodb" {
-  zone_id = var.zone_id
-  name    = "mongodb-dev.vinithaws.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mongodb.private_ip]
-}
-resource "aws_instance" "catalogue" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.security_groups
-
-  tags = {
-    Name = "catalogue"
-  }
-}
-resource "aws_route53_record" "catalogue" {
-  zone_id = var.zone_id
-  name    = "catalogue-dev.vinithaws.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.catalogue.private_ip]
-}
-resource "aws_instance" "user" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.security_groups
-
-  tags = {
-    Name = "user"
-  }
-}
-resource "aws_route53_record" "user" {
-  zone_id = var.zone_id
-  name    = "user-dev.vinithaws.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.user.private_ip]
-}
-resource "aws_instance" "cart" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.security_groups
-
-  tags = {
-    Name = "cart"
-  }
-}
-resource "aws_route53_record" "cart" {
-  zone_id = var.zone_id
-  name    = "cart-dev.vinithaws.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.cart.private_ip]
-}
-
-resource "aws_instance" "redis" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.security_groups
-
-  tags = {
-    Name = "redis"
-  }
-}
-resource "aws_route53_record" "redis" {
-  zone_id = var.zone_id
-  name    = "redis-dev.vinithaws.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.redis.private_ip]
-}
-
-resource "aws_instance" "mysql" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.security_groups
-
-  tags = {
-    Name = "mysql"
-  }
-}
-resource "aws_route53_record" "mysql" {
-  zone_id = var.zone_id
-  name    = "mysql-dev.vinithaws.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mysql.private_ip]
-}
-
-
-resource "aws_instance" "shipping" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.security_groups
-
-  tags = {
-    Name = "shipping"
-  }
-}
-resource "aws_route53_record" "shipping" {
-  zone_id = var.zone_id
-  name    = "shipping-dev.vinithaws.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.shipping.private_ip]
-}
-
-resource "aws_instance" "rabbitmq" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.security_groups
-
-  tags = {
-    Name = "rabbitmq"
-  }
-}
-resource "aws_route53_record" "rabbitmq" {
-  zone_id = var.zone_id
-  name    = "rabbitmq-dev.vinithaws.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.rabbitmq.private_ip]
-}
-
-
-resource "aws_instance" "payment" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.security_groups
-
-  tags = {
-    Name = "payment"
-  }
-}
-resource "aws_route53_record" "payment" {
-  zone_id = var.zone_id
-  name    = "payment-dev.vinithaws.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.payment.private_ip]
-}
-
-resource "aws_instance" "dispatch" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.security_groups
-  tags = {
-    Name = "dispatch"
-  }
-}
-resource "aws_route53_record" "dispatch" {
-  zone_id = var.zone_id
-  name    = "dispatch-dev.vinithaws.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.dispatch.private_ip]
+  records = [lookup(lookup(aws_instance.instance, each.key, null), "private_ip", null]
 }
