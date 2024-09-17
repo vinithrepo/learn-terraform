0c9a655774bebaa5434c4d01f5d6b63b8e7af79d
Vinith Amma <vinithamma007@gmail.com>
2024-09-17 08:30:14 +0530

    local module

--- 10-Module/local-module/main.tf
+++ 10-Module/local-module/main.tf
@@ -7,3 +7,13 @@ data "aws_ami" "ami" {
 output "ami" {
   value = "data.aws_ami.ami.id"
 }
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ami.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
+
+  tags = {
+    Name = "server1"
+  }
+}
+variable "instance_type" {}
--- 10-Module/main.tf
+++ 10-Module/main.tf
@@ -1,6 +1,11 @@
 module "test" {
   source = "./local-module"
+
 }
+
 output "test" {
   value = module.test
+}
+variable "instance_type" {
+  default = "t3.micro"
 }
