2fbe5759f6ac05be912b2da90bd6e24a7cb90a2b
Vinith Amma <vinithamma007@gmail.com>
2024-09-21 14:24:15 +0530

    v3 tf Ansible commands

--- roboshop-v3/local-module/main.tf
+++ roboshop-v3/local-module/main.tf
@@ -1,27 +1,31 @@
-data "aws_ami" "ami" {
-  most_recent = true
-  name_regex = "Centos-8-DevOps-Practice"
-  owners = [ 973714476881 ]
-}
 
 resource "aws_instance" "instance" {
-  ami           = data.aws_ami.ami.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.security_groups
+  ami                     = data.aws_ami.ami.id
+  instance_type           = var.instance_type
+  vpc_security_group_ids  = var.security_groups
 
   tags = {
     Name = var.name
   }
 }
 resource "aws_route53_record" "record" {
-  zone_id = var.zone_id
-  name    = "${var.name}-dev.vinithaws.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.instance.private_ip]
+  zone_id    = var.zone_id
+  name       = "${var.name}-dev.vinithaws.online"
+  type       = "A"
+  ttl        = 30
+  records    = [aws_instance.instance.private_ip]
 }
 
-variable "instance_type" {}
-variable "zone_id" {}
-variable "name" {}
-variable "security_groups" {}
+resource "null_resource" "ansible" {
+  depends_on = [ aws_route53_record.record ]
+  provisioner "local-exec" {
+    command = <<EOF
+
+cd /home/centos/roboshop-ansible
+git pull
+sleep 60
+ansible-playbook -i ${var.name}-dev.vinithaws.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 component=${var.name}
+
+EOF
+  }
+}
