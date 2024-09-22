afbd267c15ac30370634bc29c7480c4c93d01a1d
Vinith Amma <vinithamma007@gmail.com>
2024-09-22 13:07:44 +0530

    state s3

--- 08-tfvars/main.tf
+++ 08-tfvars/main.tf
@@ -1,10 +1,4 @@
-terraform {
-  backend "s3" {
-    bucket         = "tf.state.test"
-    key            = "tfvars/state.tf"
-    region         = "us-east-1"
-  }
-}
+
 variable "test" {}
 
 output "test" {
---
+++ 09-state/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket         = "tf.state.test"
+    key            = "tfvars/state.tf"
+    region         = "us-east-1"
+  }
+}
+variable "test" {
+  default = "hello"
+}
+
+output "test" {
+  value = var.test
+}
