4b7031b5adaa262d04131a8520f9e6e6b2375b8e
Vinith Amma <vinithamma007@gmail.com>
2024-11-22 22:33:26 +0530

    tf practice

--- intro/test.tf
+++ intro/test.tf
@@ -1,9 +1,9 @@
-
+//1
 output "xyz" {
   value = "Hello world"
 }
 
-
+//2
 variable "test" {
   default = "vinith"
 }
@@ -11,11 +11,23 @@ variable "test" {
 output "test2" {
   value = var.test
 }
-
+//3 string variable com plain variable
 variable "fruits" {
   default = "orange"
 }
 output "fruit" {
   value = var.fruits
 }
+//4 List variable
+
+variable "names" {
+  default = [
+    "surya",
+    "vinay"
+  ]
+}
+output "names"{
+  value =  var.names
+}
+
 
