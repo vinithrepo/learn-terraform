1e63b5caafc1a2f26f6677b37a480e4c6c53e228
Vinith Amma <vinithamma007@gmail.com>
2024-11-23 19:46:40 +0530

    tf practice

--- Practice/v1/main.tf
+++ Practice/v1/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "frontend" {
   ami           = "ami-0b4f379183e5706b9"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
@@ -14,5 +14,23 @@ resource "aws_route53_record" "record" {
   type    = "A"
   zone_id = "Z0345275C3S6UDSOR4CU"
   ttl = 30
-  records = [ aws_instance.web.private_ip ]
+  records = [ aws_instance.frontend.private_ip ]
 }
+resource "aws_instance" "mongodb" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
+
+
+  tags = {
+    Name = "mongodb"
+  }
+}
+
+  resource "aws_route53_record" "record" {
+    name    = "mongodb.vinithaws.online"
+    type    = "A"
+    zone_id = "Z0345275C3S6UDSOR4CU"
+    ttl = 30
+    records = [ aws_instance.mongodb.private_ip ]
+    }
