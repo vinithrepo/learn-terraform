9897c90c4190d1a46ccda72c8605b5ce9f47251f
Vinith Amma <vinithamma007@gmail.com>
2024-11-23 19:30:33 +0530

    tf practice

--- Practice/v1/main.tf
+++ Practice/v1/main.tf
@@ -1,12 +1,18 @@
 resource "aws_instance" "web" {
   ami           = "ami-0b4f379183e5706b9"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
+
 
   tags = {
     Name = "frontend"
   }
 }
 
-output "fronten_instance" {
-  value = aws_instance.web
+resource "aws_route53_record" "record" {
+  name    = "frontend"
+  type    = "A"
+  zone_id = "Z0345275C3S6UDSOR4CU"
+  ttl = 30
+  records = [ aws_instance.web.private_ip ]
 }
