4b873ba7d86b9f4f80b887aa26affa693464b7f2
Vinith Amma <vinithamma007@gmail.com>
2024-11-23 20:07:39 +0530

    tf practice

--- Practice/v1/main.tf
+++ Practice/v1/main.tf
@@ -1,7 +1,20 @@
+variable "ami_id" {
+  default = "ami-0b4f379183e5706b9"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "vpc_security_group_ids" {
+  default = ["sg-0ad8ec6873fafd140"]
+}
+variable "zone_id" {
+  default = "Z0345275C3S6UDSOR4CU"
+}
+
 resource "aws_instance" "frontend" {
-  ami           = "ami-0b4f379183e5706b9"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
 
 
   tags = {
@@ -10,16 +23,16 @@ resource "aws_instance" "frontend" {
 }
 
 resource "aws_route53_record" "frontend" {
-  name    = "frontend"
+  name    = "frontend.vinithaws.online"
   type    = "A"
-  zone_id = "Z0345275C3S6UDSOR4CU"
+  zone_id = var.zone_id
   ttl = 30
   records = [ aws_instance.frontend.private_ip ]
 }
 resource "aws_instance" "mongodb" {
-  ami           = "ami-0b4f379183e5706b9"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-0ad8ec6873fafd140"]
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
 
 
   tags = {
@@ -30,7 +43,7 @@ resource "aws_instance" "mongodb" {
   resource "aws_route53_record" "mongodb" {
     name    = "mongodb.vinithaws.online"
     type    = "A"
-    zone_id = "Z0345275C3S6UDSOR4CU"
+    zone_id = var.zone_id
     ttl = 30
     records = [ aws_instance.mongodb.private_ip ]
     }
