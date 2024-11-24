9ebf738b995b54045764b32c094a32cedf4545ec
Vinith Amma <vinithamma007@gmail.com>
2024-11-24 10:49:17 +0530

    tf practice

--- ec2creation/test1.tf
+++ ec2creation/test1.tf
@@ -8,3 +8,14 @@ output "test" {
 output "test2" {
     value = try(var.fruits["juice"], 0)
 }
+
+variable "fruits_map" {
+    default = {
+        apple = 100
+        banana = 500
+    }
+}
+
+output "test3" {
+    value = try(var.fruits_map["banana"], NA)
+}
