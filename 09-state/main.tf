188d791a96bb462a05ca285b56a49512c85d2c10
Vinith Amma <vinithamma007@gmail.com>
2025-02-14 23:01:09 +0530

    tf practice

--- 09-state/main.tf
+++ 09-state/main.tf
@@ -1,10 +1,4 @@
-terraform {
-  backend "s3" {
-    bucket         = "qwerty83"
-    key            = "tfvars/state1.tf"
-    region         = "us-east-1"
-  }
-}
+
 variable "test" {
   default = "hello"
 }
